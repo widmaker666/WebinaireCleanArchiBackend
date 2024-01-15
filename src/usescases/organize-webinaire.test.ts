06608c6ff7a370dc598fe180edff1aa7ab8f9b2c
widmaker666 <alexandre.hontcharouk69@gmail.com>
2024-01-15 15:33:09 +0100

    règle de gestion fini

--- src/usescases/organize-webinaire.test.ts
+++ src/usescases/organize-webinaire.test.ts
@@ -2,6 +2,8 @@ import { OrganizeWebinaire } from './organize-webinaires';
 import { InMemoryWebinaireRepository } from 'src/adapter/in-memory-webinaire-repository';
 import { FixedIDGenerator } from 'src/adapter/fixed-id-generator';
 import { Webinaire } from '../entities/webinaire.entity';
+import { IDateGenerator } from 'src/ports/date-generator.interface';
+import { FixedDateGenerator } from 'src/adapter/fixed-date-generator';
 
 describe('Feature: My first webinaire', () => {
   function expectWebinaireToEqual(Webinaire: Webinaire) {
@@ -16,12 +18,14 @@ describe('Feature: My first webinaire', () => {
 
   let repository: InMemoryWebinaireRepository;
   let idGenerator: FixedIDGenerator;
+  let dateGenerator: IDateGenerator;
   let useCase: OrganizeWebinaire;
 
   beforeEach(() => {
     repository = new InMemoryWebinaireRepository();
     idGenerator = new FixedIDGenerator();
-    useCase = new OrganizeWebinaire(repository, idGenerator);
+    dateGenerator = new FixedDateGenerator();
+    useCase = new OrganizeWebinaire(repository, idGenerator, dateGenerator);
   });
 
   describe('Scenario: happy path', () => {
@@ -44,6 +48,71 @@ describe('Feature: My first webinaire', () => {
 
       const createdWebinaire = repository.database[0];
       expectWebinaireToEqual(createdWebinaire);
+    });
+  });
+
+  describe('Scenario: teh webinaire happens too soon', () => {
+    const payload = {
+      title: 'My first webinaire',
+      seats: 100,
+      startDate: new Date('2023-01-01T10:00:00.000Z'),
+      endDate: new Date('2023-01-01T11:00:00.000Z'),
+    };
+    it('should throw an error', async () => {
+      await expect(() => useCase.execute(payload)).rejects.toThrowError(
+        'the webinaire must happen in at least 3 day',
+      );
+    });
+
+    it('should not create a webinaire', async () => {
+      try {
+        await useCase.execute(payload);
+      } catch (error) {}
+      expect(repository.database.length).toBe(0);
+    });
+  });
+
+  describe('Scenario: teh webinaire has too many seats', () => {
+    const payload = {
+      title: 'My first webinaire',
+      seats: 1001,
+      startDate: new Date('2023-01-10T10:00:00.000Z'),
+      endDate: new Date('2023-01-10T11:00:00.000Z'),
+    };
+
+    it('should throw an error', async () => {
+      await expect(() => useCase.execute(payload)).rejects.toThrowError(
+        'the webinaire must have a maximum of 1000 seats',
+      );
+    });
+
+    it('should not create a webinaire', async () => {
+      try {
+        await useCase.execute(payload);
+      } catch (error) {}
+      expect(repository.database.length).toBe(0);
+    });
+  });
+
+  describe('Scenario: the webinaire dont have enough seats', () => {
+    const payload = {
+      title: 'My first webinaire',
+      seats: 0,
+      startDate: new Date('2023-01-10T10:00:00.000Z'),
+      endDate: new Date('2023-01-10T11:00:00.000Z'),
+    };
+
+    it('should throw an error', async () => {
+      await expect(() => useCase.execute(payload)).rejects.toThrowError(
+        'the webinaire must at least 1 seat',
+      );
+    });
+
+    it('should not create a webinaire', async () => {
+      try {
+        await useCase.execute(payload);
+      } catch (error) {}
+      expect(repository.database.length).toBe(0);
     });
   });
 });
--- src/usescases/organize-webinaires.ts
+++ src/usescases/organize-webinaires.ts
@@ -1,4 +1,6 @@
+import { differenceInDays } from 'date-fns';
 import { Webinaire } from 'src/entities/webinaire.entity';
+import { IDateGenerator } from 'src/ports/date-generator.interface';
 import { IIDGenerator } from 'src/ports/id-genrator.interface';
 import { IWebinaireRepository } from 'src/ports/webinaire-repository.interface';
 
@@ -6,6 +8,7 @@ export class OrganizeWebinaire {
   constructor(
     private readonly repository: IWebinaireRepository,
     private readonly idGenerator: IIDGenerator,
+    private readonly dateGenerator: IDateGenerator,
   ) {}
   async execute(data: {
     title: string;
@@ -14,15 +17,27 @@ export class OrganizeWebinaire {
     endDate: Date;
   }) {
     const id = this.idGenerator.generate();
-    this.repository.create(
-      new Webinaire({
-        id,
-        title: data.title,
-        seats: data.seats,
-        startDate: data.startDate,
-        endDate: data.endDate,
-      }),
-    );
+    const webinaire = new Webinaire({
+      id,
+      title: data.title,
+      seats: data.seats,
+      startDate: data.startDate,
+      endDate: data.endDate,
+    });
+    if (webinaire.isTooClose(this.dateGenerator.now())) {
+      throw new Error('the webinaire must happen in at least 3 day');
+    }
+
+    if (webinaire.hasTooManySeats()) {
+      throw new Error('the webinaire must have a maximum of 1000 seats');
+    }
+
+    if (webinaire.hasNoSeats()) {
+      throw new Error('the webinaire must at least 1 seat');
+    }
+
+    await this.repository.create(webinaire);
+
     return { id };
   }
 }
